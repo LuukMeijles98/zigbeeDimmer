db715c29c696cd4aebfdbee92d8f711441e98a22
Luuk Meijles <luuk.meijles@zonneplan.nl>
2023-11-28 14:02:52 +0100

    test for timer

--- app/src/main.c
+++ app/src/main.c
@@ -13,18 +13,13 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
-#define SLEEP_TIME_MS 1000
 #define PWM_PERIOD 20000
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 
-int main(void){
-	printk("Starting src")	
-	
-	int ret;
-
-	if (!gpio_is_ready_dt(&led)) {
+int configLeds(){
+	if (!gpio_is_ready_dt(&led0)) {
 		printk("Error: DBG led is not ready");
 		return 0;
 	}
@@ -35,7 +30,9 @@ int main(void){
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	int ret;
+
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
@@ -46,12 +43,30 @@ int main(void){
 		return 0;
 	}
 
-	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
+	return 1;
+}
+
+void toggleLed(struct k_timer *dummy){
+		int ret = gpio_pin_toggle_dt(&led0);
 		if (ret < 0) {
 			return 0;
 		}
-		k_msleep(SLEEP_TIME_MS);
+		return 1;
+}
+
+K_TIMER_DEFINE(my_timer, toggleLed, NULL);
+
+int main(void){
+	printk("Starting led dimmer")	
+
+	if(!configLeds()){
+		printk("An error occured while configuring the LEDs\n");
+	}
+	
+	k_timer_start(&my_timer, K_SECONDS(1), K_SECONDS(1));
+
+	while (1) {
+		k_msleep(1000);
 	}
 
 	return 0;
