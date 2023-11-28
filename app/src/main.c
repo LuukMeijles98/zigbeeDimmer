bd2b7685ef0f429249f4f51ec193f45de01423a2
Luuk Meijles <luuk-meijles@hotmail.com>
2023-11-28 20:04:37 +0100

    fixes for it to work

--- app/src/main.c
+++ app/src/main.c
@@ -14,17 +14,17 @@
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 #define SLEEP_TIME_MS 1000
-#define PWM_PERIOD 20000
+#define PWM_PERIOD 200
 
 static const struct gpio_dt_spec led0 = GPIO_DT_SPEC_GET(DT_ALIAS(led0), gpios);
 static const struct pwm_dt_spec pwm_led0 = PWM_DT_SPEC_GET(DT_ALIAS(pwm_led0));
 
 int main(void){
-	printk("Starting src")	
+	printk("Starting src\n");	
 	
 	int ret;
 
-	if (!gpio_is_ready_dt(&led)) {
+	if (!gpio_is_ready_dt(&led0)) {
 		printk("Error: DBG led is not ready");
 		return 0;
 	}
@@ -35,19 +35,19 @@ int main(void){
 		return 0;
 	}
 
-	ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+	ret = gpio_pin_configure_dt(&led0, GPIO_OUTPUT_ACTIVE);
 	if (ret < 0) {
 		return 0;
 	}
 
-	ret = pwm_set_dt(&pwm_led0, PWM_PERIOD, PWM_PERIOD / 2U);
+	ret = pwm_set_dt(&pwm_led0, 1000, 200);
 	if (ret) {
 		printk("Error %d: failed to set pulse width\n", ret);
 		return 0;
 	}
 
 	while (1) {
-		ret = gpio_pin_toggle_dt(&led);
+		ret = gpio_pin_toggle_dt(&led0);
 		if (ret < 0) {
 			return 0;
 		}
